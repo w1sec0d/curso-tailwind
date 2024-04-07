3eaa066bc2802bb13d648ca9df662a5be3309055
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-07 00:25:24 -0500

    Añade README y perfecciona final video 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Curso Tailwind Codewi
+
+- Este es el código Fuente del [curso de Tailwind de Codewi](https://www.youtube.com/playlist?list=PL-pltyODNS3dwxLzu-bmrjVn2QznWeKcb)
+
+- Encontrarás en este repositorio una carpeta por cada video, con código fuente, en ocaciones de codigo de inicio del video y código final del video. Además de otros recursos necesarios
--- video1/codigo/src/App.jsx
+++ video1/codigo/src/App.jsx
@@ -1,4 +1,5 @@
 import Button from "./components/Button/Button";
+import tailwindLogo from "./assets/tailwindlogo2.png";
 
 function saludar() {
   alert("Hola, soy un botón");
@@ -7,7 +8,18 @@ function saludar() {
 function App() {
   return (
     <div className="flex items-center justify-center w-screen flex-col">
-      <p className="text-blue">Hello Tailwind</p>
+      <img
+        src={tailwindLogo}
+        alt="Tailwind Logo"
+        className="w-[200px] hover:translate-x-1 hover:translate-y-1 hover:scale-110 transition-all"
+      />
+      <p className="text-blue text-6xl my-7">
+        ¡Hello{" "}
+        <span className="bg-[#fbfbfb] text-[#38bdf8] font-bold px-3 mr-3 hover:text-[69px] transition-all">
+          Tailwind
+        </span>
+        !
+      </p>
       <Button onClick={saludar}>Soy un botón</Button>
       <a
         href="https://www.youtube.com/@codewi?sub_confirmation=1"
@@ -18,7 +30,7 @@ function App() {
           Suscríbete
         </Button>
       </a>
-      <a href="https://www.tiktok.com/@codewi" target="_blank" rel="noreferrer">
+      <a href="https://tiktok.com/@codewi" target="_blank" rel="noreferrer">
         <Button className="bg-green-600 hover:bg-green-700 active:bg-green-800 active:ring active:ring-green-300">
           Codewi
         </Button>
