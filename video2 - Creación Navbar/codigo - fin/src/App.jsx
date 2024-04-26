826f16e89e074fb52522780fe2761be05cc25b30
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-25 23:09:08 -0500

    Añade componente navbar básico y altera componente root

--- video2 - Creación Navbar/codigo - fin/src/App.jsx
+++ video2 - Creación Navbar/codigo - fin/src/App.jsx
@@ -1,5 +1,6 @@
 import Button from "./components/Button/Button";
 import tailwindLogo from "./assets/tailwindlogo2.png";
+import NavBar from "./components/Navbar/Navbar";
 
 function saludar() {
   alert("Hola, soy un botón");
@@ -7,35 +8,40 @@ function saludar() {
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-screen flex-col">
-      <img
-        src={tailwindLogo}
-        alt="Tailwind Logo"
-        className="w-[200px] hover:translate-x-1 hover:translate-y-1 hover:scale-110 transition-all"
-      />
-      <p className="text-blue text-6xl my-7">
-        ¡Hello{" "}
-        <span className="bg-[#fbfbfb] text-[#38bdf8] font-bold px-3 mr-3 hover:text-[69px] transition-all">
-          Tailwind
-        </span>
-        !
-      </p>
-      <Button onClick={saludar}>Soy un botón</Button>
-      <a
-        href="https://www.youtube.com/@codewi?sub_confirmation=1"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Button className="bg-red-600 hover:bg-red-700 active:bg-red-800 active:ring active:ring-red-300">
-          Suscríbete
-        </Button>
-      </a>
-      <a href="https://tiktok.com/@codewi" target="_blank" rel="noreferrer">
-        <Button className="bg-green-600 hover:bg-green-700 active:bg-green-800 active:ring active:ring-green-300">
-          Codewi
-        </Button>
-      </a>
-    </div>
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main className="flex items-center justify-center mx-auto flex-col h-screen">
+        <img
+          src={tailwindLogo}
+          alt="Tailwind Logo"
+          className="w-[200px] hover:translate-x-1 hover:translate-y-1 hover:scale-110 transition-all"
+        />
+        <p className="text-blue text-6xl my-7">
+          ¡Hello{" "}
+          <span className="bg-[#fbfbfb] text-[#38bdf8] font-bold px-3 mr-3 hover:text-[69px] transition-all">
+            Tailwind
+          </span>
+          !
+        </p>
+        <Button onClick={saludar}>Soy un botón</Button>
+        <a
+          href="https://www.youtube.com/@codewi?sub_confirmation=1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button className="bg-red-600 hover:bg-red-700 active:bg-red-800 active:ring active:ring-red-300">
+            Suscríbete
+          </Button>
+        </a>
+        <a href="https://tiktok.com/@codewi" target="_blank" rel="noreferrer">
+          <Button className="bg-green-600 hover:bg-green-700 active:bg-green-800 active:ring active:ring-green-300">
+            Codewi
+          </Button>
+        </a>
+      </main>
+    </>
   );
 }
 
---
+++ video2 - Creación Navbar/codigo - fin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,3 @@
+const NavBar = () => <nav>Soy una barra de navegación!</nav>;
+
+export default NavBar;
