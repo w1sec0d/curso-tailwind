61c821eb8193ed2ef0d39201152da0270b4f0727
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-25 23:57:03 -0500

    Añade estilo reesponsive básico

--- video2 - Creación Navbar/codigo - fin/src/components/Navbar/Navbar.jsx
+++ video2 - Creación Navbar/codigo - fin/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,41 @@
 import Button from "../Button/Button";
-import Menu from iconoi
+import { Menu, SoccerBall } from "iconoir-react";
+import { useRef } from "react";
 
 const NavbarLink = ({ children }) => (
-  <li className="mr-5 hover:cursor-pointer font-semibold hover:border-b-white hover:border-b-2 transition-all h-full py-4">
+  <a className="lg:mr-5 hover:cursor-pointer font-semibold hover:border-b-white hover:border-b-2 transition-all h-full py-4">
     {children}
-  </li>
+  </a>
 );
 
-const NavBar = () => (
-  <nav className="bg-neutral-900 h-[60px] flex flex-row items-center justify-between px-4">
-    <ul className="flex flex-row items-center">
-      <NavbarLink>Inicio</NavbarLink>
-      <NavbarLink>Sobre Nosotros</NavbarLink>
-    </ul>
-    <div>
-      <Button className="mt-0 mx-3">Registrarse</Button>
-      <Button className="mt-0">Login</Button>
+const NavBar = () => {
+  const menuRef = useRef();
+  const showMenu = () => {
+    menuRef.current.classList.toggle("-top-[220px]");
+  };
 
-    </div>
-  </nav>
-);
+  return (
+    <nav className="bg-neutral-900 h-[60px] fixed left-0 top-0 w-screen">
+      <div className="flex flex-row items-center justify-between bg-neutral-900 px-4 h-full">
+        <div className="flex flex-row">
+          <SoccerBall className="translate-y-1"></SoccerBall>
+          <p className="ml-3 font-bold text-lg">Fulvo</p>
+        </div>
+        <div
+          className="flex absolute lg:static -top-[220px] lg:ml-auto lg:top-0 lg:flex-row flex-col justify-center items-center lg:items-center"
+          ref={menuRef}
+        >
+          <NavbarLink>Inicio</NavbarLink>
+          <NavbarLink>Sobre Nosotros</NavbarLink>
+          <div className="flex flex-row items-center h-full">
+            <Button className="my-4 lg:my-0 !mt-0 lg:mx-3">Registrarse</Button>
+            <Button className="my-4 lg:my-0 !mt-0">Login</Button>
+          </div>
+        </div>
+        <Menu className="lg:hidden" onClick={showMenu} />
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
