c09e2e689a61e15dd211ee340636d9840807a3a0
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-25 23:31:08 -0500

    Añade estilos de navbar desktop básico

--- video2 - Creación Navbar/codigo - fin/src/components/Navbar/Navbar.jsx
+++ video2 - Creación Navbar/codigo - fin/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,24 @@
-const NavBar = () => <nav>Soy una barra de navegación!</nav>;
+import Button from "../Button/Button";
+import Menu from iconoi
+
+const NavbarLink = ({ children }) => (
+  <li className="mr-5 hover:cursor-pointer font-semibold hover:border-b-white hover:border-b-2 transition-all h-full py-4">
+    {children}
+  </li>
+);
+
+const NavBar = () => (
+  <nav className="bg-neutral-900 h-[60px] flex flex-row items-center justify-between px-4">
+    <ul className="flex flex-row items-center">
+      <NavbarLink>Inicio</NavbarLink>
+      <NavbarLink>Sobre Nosotros</NavbarLink>
+    </ul>
+    <div>
+      <Button className="mt-0 mx-3">Registrarse</Button>
+      <Button className="mt-0">Login</Button>
+
+    </div>
+  </nav>
+);
 
 export default NavBar;
